3e26dfe312d3621454a6873751095082fd8aaa3f
Antoniuk Viktoriia <antoniuk.viktoriia11@gmail.com>
2025-03-24 21:55:50 +0200

    pow

--- task.js
+++ task.js
@@ -1,37 +1,8 @@
-'number' + 3 + 3
-
-null + 3
-
-5 && "qwerty"
-
-+'40' + +'2' + "hillel";
-
-'10' - 5 === 6;
-
-true + false
-
-'4px' - 3
-
-'4' - 3
-
-'6' + 3 ** 0;
-
-12 / '6'
-
-'10' + (5 === 6);
-
-null == ''
-
-3 ** (9 / 3);
-
-!!'false' == !!'true'
-
-0 || '0' && 1
-
-(+null == false) < 1;
-
-false && true || true
-
-false && (false || true);
-
-(+null == false) < 1 ** 5;
+function pow(x, y) {
+    let result = 1;
+    for (let i = 0; i < y; i++) {
+        result *= x;
+    }
+    return result;
+}
+console.log(pow(11, 5))
