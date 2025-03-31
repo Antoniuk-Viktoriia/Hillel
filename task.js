1a13806a446f43f7c176e1a616fbb3f1baeaaed3
Antoniuk Viktoriia <antoniuk.viktoriia11@gmail.com>
2025-03-31 13:50:56 +0300

    homework4

--- task.js
+++ task.js
@@ -1,8 +1,18 @@
-function pow(x, y) {
-    let result = 1;
-    for (let i = 0; i < y; i++) {
-        result *= x;
+function checkProbabilityTheory(count) {
+    let evenCount = 0;
+    let oddCount = 0;
+
+    for (let i = 0; i < count; i++) {
+        let randomNumber = 
+        Math.floor(Math.random() *  (1000 - 100 + 1)) + 100;
+        if (randomNumber % 2 === 0) {
+                evenCount++;
+        } else {
+                oddCount++;
+        }
     }
-    return result;
+    let evenPercentage = (evenCount / count * 100).toFixed(2);
+    let oddPercentage = (oddCount / count * 100).toFixed(2);
+    return `Кількість згенерованих чисел: ${count}\nПарних чисел: ${evenCount}\nНепарних чисел: ${oddCount}\nВідсоток парних до непарних: ${evenPercentage}% / ${oddPercentage}%;` 
 }
-console.log(pow(11, 5))
+console.log(checkProbabilityTheory(11));
