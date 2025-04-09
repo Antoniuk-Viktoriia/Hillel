9c8f3a3449d69eda12d30a33c681b20d3021fcca
Antoniuk Viktoriia <antoniuk.viktoriia11@gmail.com>
2025-04-09 21:46:56 +0300

    Homework 5

--- task.js
+++ task.js
@@ -1,18 +1,38 @@
-function checkProbabilityTheory(count) {
-    let evenCount = 0;
-    let oddCount = 0;
-
-    for (let i = 0; i < count; i++) {
-        let randomNumber = 
-        Math.floor(Math.random() *  (1000 - 100 + 1)) + 100;
-        if (randomNumber % 2 === 0) {
-                evenCount++;
-        } else {
-                oddCount++;
-        }
+var services = {
+    "стрижка": "60грн",
+    "гоління": "80грн",
+    "миття голови": "100грн"
+  };
+  
+  services["розбите скло"] = "200грн";
+  
+  var serviceUtils = {
+    getPrices: function() {
+      return Object.values(services).map(function(value) {
+        return parseInt(value); 
+      });
+    },
+  
+    price: function() {
+      var prices = this.getPrices();
+      var total = 0;
+      for (var i = 0; i < prices.length; i++) {
+        total += prices[i];
+      }
+      return total;
+    },
+  
+    minPrice: function() {
+      var prices = this.getPrices();
+      return Math.min.apply(null, prices);
+    },
+  
+    maxPrice: function() {
+      var prices = this.getPrices();
+      return Math.max.apply(null, prices);
     }
-    let evenPercentage = (evenCount / count * 100).toFixed(2);
-    let oddPercentage = (oddCount / count * 100).toFixed(2);
-    return `Кількість згенерованих чисел: ${count}\nПарних чисел: ${evenCount}\nНепарних чисел: ${oddCount}\nВідсоток парних до непарних: ${evenPercentage}% / ${oddPercentage}%;` 
-}
-console.log(checkProbabilityTheory(11));
+  };
+  
+  console.log("Загальна вартість: " + serviceUtils.price() + "грн");
+  console.log("Мінімальна ціна: " + serviceUtils.minPrice() + "грн");
+  console.log("Максимальна ціна: " + serviceUtils.maxPrice() + "грн");
